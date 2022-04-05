8003bd00f66c47505fc357e5c6ebd12c636727ba
elijahsamuels <elijahsamuels@gmail.com>
2022-04-04 23:01:09 -0600

    updated contact page with nodemailer.js and removed the static link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Layout from '../components/layout.js';
 import SEO from '../components/seo.js';
+import ContactForm from '../components/ContactForm.js';
 
 const Contact = () => (
     <Layout>
         <SEO title='Dru Heller Contact page'/>
         <div>
-            <h1>Contact Dru</h1>
-            {/* need to create a mail form, but this will work for the moment. */}
-            <a href="mailto:druheller@gmail.com">druheller@gmail.com</a>
+            {/* <h1>Contact Dru</h1> */}
+            <h5 align='center'>
+							Hi! I would love to hear from you. Please feel free to send me a message!
+						</h5>
+            {/* <a href="mailto:druheller@gmail.com">druheller@gmail.com</a> */}
         </div>
+				< ContactForm />
     </Layout>
 )
 
