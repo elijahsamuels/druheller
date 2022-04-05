81e36650392e4ada8eaa78ebc00945880b98503d
elijahsamuels <elijahsamuels@gmail.com>
2022-04-04 23:49:02 -0600

    added react-icons for social media icons in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaFacebookSquare } from "@react-icons/all-files/fa/FaFacebookSquare";
+import { FaYoutube } from "@react-icons/all-files/fa/FaYoutube";
 
+// 
 const copyrightDate = new Date().getFullYear();
 
 const randomLinkGenerator = () => {
@@ -19,12 +22,13 @@ const Footer = () => (
     }}>
 
 			<div align="center">
-				<a href='https://www.facebook.com/dru.heller'>
-				{/* <FontAwesomeIcon icon="fab fa-facebook-square" /> */}
-				{/* <FontAwesomeIcon icon="fa-solid fa-user" /> */}
 
-					facebook
-					</a>
+
+				<a href='https://www.facebook.com/dru.heller' className="social-icons"><text hidden="true">Facebook</text><FaFacebookSquare /></a>
+				<a href='https://www.youtube.com/results?search_query=dru+heller' className="social-icons"><text hidden="true">YouTube</text><FaYoutube /></a>
+
+
+
 			</div>
         <div align="center">© {copyrightDate} by 
             <a href='https://shibadeveloper.com'> Shiba</a> & <a href='https://www.elijahsamuels.com'>Samuels</a> <a href={randomLinkGenerator()}>Development</a> 
