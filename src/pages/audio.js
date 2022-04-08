944b7ac5d98784405547e422d1f16384f7636d36
elijahsamuels <elijahsamuels@gmail.com>
2022-04-08 13:09:31 -0600

    added placeholder link and found where to host mp3 files for dru.

--- src/pages/audio.js
+++ src/pages/audio.js
@@ -3,18 +3,36 @@ import Layout from "../components/layout.js"
 import SEO from "../components/seo.js"
 import Player from "../components/player.js"
 // import Player2 from "../components/player2.js"
+import testImage from '../images/heller1.jpg';
 
+// ************************************ //
+// 																			//
+// 		UPLOAD MP3 FILES HERE:						//
+// 		https://mailboxdrive.com/upload/	//
+// 																			//
+// ************************************ //
 
 const Audio = () => (
   <Layout>
     <SEO title="Dru Heller Audio page" />
     <div>
-      <h1>Audio</h1>
+      {/* <h1>Audio</h1> */}
 
+
+      <h3 align="center">Album Cover Placeholder</h3>
+			<img className="album-img" src={testImage} alt='album cover title' />
+			<div>
+				<h3 align="center">Album Title - Track Title</h3>
+				<div className="audio">
+					<Player src={"https://www.mboxdrive.com/Another%20Way%20-%20Nitti%20Gritti%20(Sax%20Remix).mp3"}/>
+					{/* <Player src={"https://samplelib.com/lib/preview/mp3/sample-3s.mp3"}/>				 */}
+				</div>
+			</div>
+  
+      <h3 align="center">Album Title - Track Title</h3>
       <div className="audio">
-			<Player src={"https://samplelib.com/lib/preview/mp3/sample-3s.mp3"}/>				
-			{/* <Player />				 */}
-
+        <Player src={"https://www.mboxdrive.com/Another%20Way%20-%20Nitti%20Gritti%20(Sax%20Remix).mp3"}/>
+        {/* <Player src={"https://samplelib.com/lib/preview/mp3/sample-3s.mp3"}/>				 */}
       </div>
     </div>
   </Layout>
