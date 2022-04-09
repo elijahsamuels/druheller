be06e20aab2f249fc6557edb09412c42ec70b9e9
elijahsamuels <elijahsamuels@gmail.com>
2022-04-08 19:08:51 -0600

    added the Calendar component. Turned off until we can get the api key from dru.

--- src/pages/live.js
+++ src/pages/live.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import Layout from '../components/layout.js';
-import SEO from '../components/seo.js';
+import React from "react"
+import Layout from "../components/layout.jsx"
+import SEO from "../components/seo.js"
+import GoogleCalendar from "../components/calendar.js"
 // import plugins from '../components/gatsby-config.js';
 
 const Live = () => (
-    <Layout >
-        <SEO title='Dru Heller Live page'/>
-        <div align="center">
-            <h1>Live</h1>
-            <p>Gig Calendar Information</p>
-            {/* This is a placeholder of a google calendar. We need a Google calendar from Dru. */}
-            <iframe title='google-calendar' src="https://calendar.google.com/calendar/embed?src=lpv9uleqd0r413a0ccc3tdaisc%40group.calendar.google.com&ctz=America%2FDenver" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-            
-        </div>
-        <div>
-            {/* This should be the Gatsby Calendar Plugin */}
-        </div>
-    </Layout>
+  <Layout>
+    {/* <GoogleCalendar /> */}
+
+    <SEO title="Dru Heller Live page" />
+    <div align="center">
+      <h1>Live</h1>
+      <p>Gig Calendar Information</p>
+      {/* This is a placeholder of a google calendar. We need a Google calendar from Dru. */}
+      <iframe
+        title="google-calendar"
+        src="https://calendar.google.com/calendar/embed?src=lpv9uleqd0r413a0ccc3tdaisc%40group.calendar.google.com&ctz=America%2FDenver"
+        width="800"
+        height="600"
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+    <div>{/* This should be the Gatsby Calendar Plugin */}</div>
+  </Layout>
 )
 
-export default Live;
-
+export default Live
