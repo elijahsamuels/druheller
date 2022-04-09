275cbf01cd494b5e34d7b1597a43836daa9e8453
elijahsamuels <elijahsamuels@gmail.com>
2022-04-08 19:28:02 -0600

    changed menu.js logic to hopefully work with Netifly and Gatsby build

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,38 +1,66 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { useEffect } from "react"
+import Link from "gatsby-link"
 
-function onMenuChange(title) {
-	if (document.URL.includes(title.toLowerCase())) {
-		return <p style={{ color: 'white' }}>{title}</p>
-	} else {
-		return title
+
+const Menu = () => {
+
+	function onMenuChange(title) {
+		if (document.URL.includes(title?.toLowerCase())) {
+			return <p style={{ color: "white" }}>{title}</p>
+		} else {
+			return title
+		}
 	}
 
+	useEffect(() => {
+		onMenuChange()
+	}, [])
+
+  return (
+    <div
+      className="menu"
+      style={{
+        paddingTop: "20px",
+      }}
+      id="menu"
+    >
+      <ul
+        style={{
+          listStyle: "none",
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
+        {/* if url is current location, make link text white */}
+
+        <li>
+          <Link to="/about">{onMenuChange("About")}</Link>
+        </li>
+        <li>
+          <Link to="/live">{onMenuChange("Live")}</Link>
+        </li>
+        <li>
+          <Link to="/lessons">{onMenuChange("Lessons")}</Link>
+        </li>
+
+        <li>
+          <h3>
+            <Link to="/">{onMenuChange("Dru Heller")}</Link>
+          </h3>
+        </li>
+
+        <li>
+          <Link to="/audio">{onMenuChange("Audio")}</Link>
+        </li>
+        <li>
+          <Link to="/video">{onMenuChange("Video")}</Link>
+        </li>
+        <li>
+          <Link to="/contact">{onMenuChange("Contact")}</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
-const Menu = () => (
-    <div className="menu" style={{
-        paddingTop: '20px'
-    }} id='menu'>
-        <ul style={{
-            listStyle: 'none',
-            display: 'flex',
-            justifyContent: 'space-evenly',
-        }}>
-
-            {/* if url is current location, make link text white */}
-
-            <li><Link to='/about'>{onMenuChange("About")}</Link></li> 
-            <li><Link to='/live'>{onMenuChange("Live")}</Link></li> 
-            <li><Link to='/lessons'>{onMenuChange("Lessons")}</Link></li> 
-
-            <li><h3><Link to='/'>{onMenuChange("Dru Heller")}</Link></h3></li>
-            
-            <li><Link to='/audio'>{onMenuChange("Audio")}</Link></li> 
-            <li><Link to='/video'>{onMenuChange("Video")}</Link></li> 
-            <li><Link to='/contact'>{onMenuChange("Contact")}</Link></li> 
-        </ul>        
-    </div>
-)
-
-export default Menu;
+export default Menu
